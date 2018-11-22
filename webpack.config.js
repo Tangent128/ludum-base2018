7e97e72cf288abcf7927e4fe8156903a06360027
Tangent 128 <Tangent128@gmail.com>
2018-11-22 11:25:16 -0500

    Stub Typescript Webpack project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.ts',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Game Base 2018'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
