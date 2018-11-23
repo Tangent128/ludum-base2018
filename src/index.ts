39f3956648e080cf04e6d0d6b8e99bbaaa5fca4c
Tangent 128 <Tangent128@gmail.com>
2018-11-23 01:19:26 -0500

    Compile SASS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import "./index.scss";
 
 export function test() {
-    alert("Hello, World");
+    const div = document.querySelector("div");
+    if(div) div.innerText = "Hello, World!";
 }
 
 test();
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: './src/index.ts',
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: "development",
+    entry: './src/index.ts',
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              // fallback to style-loader in development
+              isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+              "css-loader",
+              "sass-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html"
+      }),
+      new MiniCssExtractPlugin({})
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  }
 };
